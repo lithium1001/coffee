386ed83621d5487adf5133145cea8b13c631f177
Ziyuan Zhou <87697007+ZiyuanZhou901@users.noreply.github.com>
2023-07-22 09:22:37 +0800

    debug

--- Coffee/src/main/java/com/example/coffee/Jwt/JwtAuthenticationFilter.java
+++ Coffee/src/main/java/com/example/coffee/Jwt/JwtAuthenticationFilter.java
@@ -33,15 +33,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private boolean isProtectedUrl(HttpServletRequest request) {
         List<String> protectedPaths = new ArrayList<String>();
-        protectedPaths.add("/ums/user/info");
-        protectedPaths.add("/ums/user/update");
+        //protectedPaths.add("/user/info");
+        protectedPaths.add("/user/update");
         protectedPaths.add("/post/create");
         protectedPaths.add("/post/update");
         protectedPaths.add("/post/delete/*");
         protectedPaths.add("/comment/add_comment");
-        protectedPaths.add("/relationship/subscribe/*");
-        protectedPaths.add("/relationship/unsubscribe/*");
-        protectedPaths.add("/relationship/validate/*");
 
         boolean bFind = false;
         for( String passedPath : protectedPaths ) {
